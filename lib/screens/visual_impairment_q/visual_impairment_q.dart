c7c21e298bf4028019fd3567173081467803a9c2
daliaawad <awaddalia60@gmail.com>
2023-04-14 14:36:27 +0300

    Update the tts function

--- lib/screens/visual_impairment_q/visual_impairment_q.dart
+++ lib/screens/visual_impairment_q/visual_impairment_q.dart
@@ -25,45 +25,13 @@ class _VisualImpairmentQState extends State<VisualImpairmentQ> {
                   image: AssetImage("assets/images/visual_impairment_q.png"),
                   fit: BoxFit.fill),
             ),
-            child: TextToSpeech() /* add child content here */,
+            child: Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.fromLTRB(30, 155, 30, 0),
+              child: const TextField(
+                  decoration: InputDecoration(hintText: 'Answer')),
+            ),
           )
-          // SafeArea(
-          //   child: Column(
-          //     crossAxisAlignment: CrossAxisAlignment.start,
-          //     children: [
-          //       Spacer(flex: 2), //2/6
-          //       // Text(
-          //       //   "Let's Play Quiz,",
-          //       //   style: Theme.of(context).textTheme.headline4,
-          //       // ),
-          //       //Text("Enter your informations below"),
-          //       Spacer(), // 1/6
-          //       TextField(
-          //         decoration: InputDecoration(
-          //           filled: true,
-          //           fillColor: Color(0xFF1C2341),
-          //           hintText: "Full Nam",
-          //           border: OutlineInputBorder(
-          //             borderRadius: BorderRadius.all(Radius.circular(12)),
-          //           ),
-          //         ),
-          //       ),
-          //       Spacer(), // 1/6
-          //       InkWell(
-          //         child: Container(
-          //           width: double.infinity,
-          //           alignment: Alignment.center,
-          //           decoration: BoxDecoration(
-          //             borderRadius: BorderRadius.all(Radius.circular(12)),
-          //           ),
-          //           //child: Text("Lets Start Quiz",
-          //           // style: Theme.of(context).textTheme.button),
-          //         ),
-          //       ),
-          //       Spacer(flex: 2), // it will take 2/6 spaces
-          //     ],
-          //   ),
-          // ),
         ],
       ),
     );
